ba8663eac9b6781ced90d33b7097e86e85cb9b58
watchbutdonotlearn <71526591+watchbutdonotlearn@users.noreply.github.com>
2021-12-10 16:54:14 -0600

    Show values on return to settings

--- popup.js
+++ popup.js
@@ -8,18 +8,16 @@ function saveAll(){
     chrome.storage.local.set({enckey: key});
 }
 
-/*
 function returnvalues(){
     chrome.storage.local.get(['tokenValue', 'enckey'], function(data){
-        console.log(data.token)
+        console.log(data.tokenValue)
         console.log(data.enckey)
-        document.getElementById('token').value = data.token
+        document.getElementById('token').value = data.tokenValue
         document.getElementById('enckey').value = data.enckey
     })
 }
-*/
 
 window.onload=function(){
     document.getElementById('saveBtn').addEventListener('click', saveAll);
-    //returnvalues();
+    returnvalues();
 }
