9fc738236b223e15f9679702c8655aa2a50321ab
watchbutdonotlearn <71526591+watchbutdonotlearn@users.noreply.github.com>
2021-12-10 12:57:29 -0600

    Add extension

---
+++ popup.js
@@ -0,0 +1,25 @@
+console.log('Something') 
+
+function saveAll(){
+    console.log('saving preferences')
+    var token = document.getElementById('token').value
+    chrome.storage.local.set({tokenValue: token});
+    var key = document.getElementById('enckey').value
+    chrome.storage.local.set({enckey: enckey});
+}
+
+/*
+function returnvalues(){
+    chrome.storage.local.get(['token', 'enckey'], function(data){
+        console.log(data.token)
+        console.log(data.enckey)
+        document.getElementById('token').value = data.token
+        document.getElementById('enckey').value = data.enckey
+    })
+}
+*/
+
+window.onload=function(){
+    document.getElementById('saveBtn').addEventListener('click', saveAll);
+    //returnvalues();
+}
